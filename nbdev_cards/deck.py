f7415fc5c3e76c941f0a96f955ab71614fc34139
Gianni Crivello <giannicrivello@Giannis-Mac-mini.local>
2024-08-03 19:47:03 -0400

    ran nbdev

--- nbdev_cards/deck.py
+++ nbdev_cards/deck.py
@@ -8,6 +8,9 @@ from .card import *
 
 # %% ../nbs/01_deck.ipynb 4
 class Deck:
+    "A deck of 52 cards, not including jokers"
     def __init__(self): self.cards = [Card(s, r) for s in range(4) for r in range(1,14)]
+    def __len__(self): return len(self.cards)
     def __str__(self): return "; ".join(map(str, self.cards))
+    def __contains__(self, card: "Deck"): return card in self.cards
     __repr__ = __str__
